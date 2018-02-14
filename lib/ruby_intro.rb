a586fe51d36fcc18a126c1e7010342c0398af574
soumyajyoti-sj <soumyajyoti.sj@gmail.com>
2018-02-13 22:44:24 -0600

    Final Submission

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,11 +2,9 @@
 
 # Part 1
 
-
 def sum arr
-  arr.reduce 0, :+
+  arr.reduce(0) { |total, element| total + element }
 end
-
 
 #-------------------------------------------------------
 
@@ -17,54 +15,57 @@ end
 #-------------------------------------------------------
 
 def sum_to_n? arr, n
-  if arr.empty?
+
+  if arr.length < 2
     return false
   else
-    arr.combination(2).to_a.each do |pair|
-      return true if sum(pair) == n
-    end
+    arr.combination(2).find{|i,j| i + j == n } ? true : false
   end
 
-  return false
 end
+
+
 
 #--------------------------------------------------------
 
 # Part 2
 
 def hello name
-  "Hello, " + name
+  "Hello, #{name}"
 end
 
 #--------------------------------------------------------
 
 def starts_with_consonant? s
-  !!(s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
+  /^[bcdfghjklmnprstvwxyz]/i.match(s) != nil
 end
 
 #--------------------------------------------------------
 
 def binary_multiple_of_4? s
-  if s =~ /^[0-1]+$/
+  if /^[0-1]+$/.match(s) != nil
     return s.to_i(2) % 4 == 0
   end
-  false
+  return false
 end
+
 
 # Part 3
 
+
 class BookInStock
 
-  attr_accessor :isbn, :price
+  attr_accessor(:isbn, :price)
 
-  def initialize isbn, price
-    raise ArgumentError if isbn.empty? || price <= 0
+  def initialize(isbn, price)
+    raise ArgumentError, "Illegal argument" if isbn.empty?  or price <= 0
     @isbn = isbn
     @price = price
   end
 
   def price_as_string
-    format("$%.2f", @price)
+    sprintf("$%2.2f", @price)
   end
-
+  
 end
+
